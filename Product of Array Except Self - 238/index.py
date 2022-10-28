ddf7a2cd07319cc57cd7ffbbed2ff6c4f8788daa
Sreejit <sreejit9821@gmail.com>
2022-10-28 12:08:21 +0530

    Prefix Postfix solution

---
+++ Product of Array Except Self - 238/index.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def productExceptSelf( nums: List[int]) -> List[int]:
+        res = [1] * (len(nums))
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        
+        return res
+
+
+
+
+productExceptSelf = Solution();
+print(Solution.productExceptSelf([1,2,3,4]))
