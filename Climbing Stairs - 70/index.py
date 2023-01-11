08f533f34a0ce8663ecfab39cf82dac4f179f566
Sreejit <sreejit9821@gmail.com>
2023-01-11 08:12:35 +0530

    Climbing stairs problem

---
+++ Climbing Stairs - 70/index.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+    one, two = 1,1
+
+    for i in range(n - 1):
+       temp = one
+       one = one + two
+       two = one
+
+   return one
